8aca9337be6e30556e2951163687a7805e62bd4f
jeffdev <jefferson-67@live.com>
2021-07-26 19:55:56 -0300

    Desafio 04 - Soma das notas dos alunos, no qual deveria ser uma classe. e usar também List e array.

---
+++ Desafio04/Desafio04/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Desafio04
+{
+    class Aluno
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public double nota { get; set; }
+
+        public void Nota()
+        {
+            Aluno[] alunos =
+            {
+            new Aluno{nome = "John Smith", idade =12, nota=7},
+            new Aluno{nome = "Ana Lee", idade =13, nota=9},
+            new Aluno{nome = "Maria Jane", idade =13, nota=8},
+            new Aluno{nome = "Alex Rose", idade =11, nota=6}
+            };
+ 
+        }
+    }
+}
