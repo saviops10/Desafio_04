06368f09fb3c1413725da7c6e14f397390aad1ef
luansoliver <rookie.qodeless@outlook.com>
2021-08-19 04:56:57 -0300

    Desafio 4

---
+++ Desafio04/Desafio04/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Desafio04
+{
+    class Aluno
+    {
+        public string nome { get; }
+        public int idade { get; }
+        public double nota { get; }
+    }
+}
---
+++ Desafio04/Desafio04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Aluno> aluno = new List<Aluno>();
+            aluno.Add(new Aluno { nome = "John Smith", idade = 12, nota = 7 });
+            aluno.Add(new Aluno { nome = "Ana Lee", idade = 13, nota = 9 });
+            aluno.Add(new Aluno { nome = "Maria Jane", idade = 13, nota = 8 });
+            aluno.Add(new Aluno { nome = "Alex Rose", idade = 11, nota = 6 });
+
+            foreach (var estudante in aluno)
+            {
+                Console.WriteLine((" " + estudante.nome + "\t"));
+                Console.WriteLine((" Idade: " + estudante.idade + "\t"));
+
+                Console.WriteLine(" Nota final: " + estudante.nota + "\t" + "\n");
+
+            }
+            int[] notas = { 7, 9, 8, 6 };
+            int soma = notas.Sum();
+            Console.WriteLine(" A Soma das notas: " + soma);
+        }
+    }
+}
